6a379ccf0d293012617dce9f9641890bd17a3e8d
Kai <sm5069@rit.edu>
2021-07-13 12:27:19 -0400

    Initial commit

---
+++ primary.py
@@ -0,0 +1,57 @@
+# author:
+# date:
+
+# --------------- Section 1 --------------- #
+
+# 1 | String Methods
+#
+# 1 - Save your name to a variable named name.
+#   a. Center that variable within 30 characters. Print it.
+#   b. Print the variable in all upper case.
+#   c. Print the variable in all lower case.
+#   d. Print the variable capitalized (Look to documentation.)
+
+
+# 2 | String Methods
+#
+# 1 - Prompt input from the user, asking them to enter a sentence. Save it to a variable named text.
+#   a. Find the first instance of the letter a. Print that position.
+#   b. Find the first instance of the letter b. Print that position.
+#   c. Find the first instance of a word of your choice. Print that position.
+
+
+# 3 | String Methods
+#
+# 1 - Prompt input from the user, asking them to enter a sentence. Save it to a variable named text.
+#   a. Find the position of every vowel in text. Save them each to a variable.
+#   b. Using a built-in function, print the position of the vowel that shows up last.
+#   c. Using a built-in function, print the position of the vowel that shows up first.
+
+
+# 4 | String Indexing
+#
+# 1 - Prompt input from the user, asking them to enter a sentence. Save it to a variable named text.
+#   a. Print the 0th letter of the text using string indexing.
+#   b. Print the 1st, 2nd, and 3rd letters of the text using string indexing.
+#   c. Print the last letter of the text using string indexing.
+#       HINT: There are multiple ways of doing this. Is there a function that we can use that will find
+#           the position of the last letter, or atleast one off from it?
+
+
+# 5 | String Slicing
+#
+# 1 - Prompt input from the user, asking them to enter a sentence. Save it to a variable named text.
+#   a. Slice text from the 2nd position to the 5th. Print that.
+#   b. Slice text from the 0th position to the 8th. Print that.
+#   c. Slice text from 3rd position to end. Print that.
+#   d. Slice text from the beginning to 5 positions before the last character. Print that.
+#       HINT: Use a function to get the last position of the string.
+
+
+# 6 | String Slicing
+#
+# 1 - Prompt input from the user, asking them to enter a sentence. Save it to a variable named text.
+#   a. Print the text, but only every 2nd character.
+#   b. Print the text, but only every 3rd character.
+#   c. Print the text, but in reverse order.
+
